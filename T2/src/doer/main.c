7a72a7e214f0df267dbc67acf053dec2f8f87373
Benjamin <rego_1994@hotmail.com>
2018-04-13 11:34:02 -0300

    Main doer

--- T2/src/doer/main.c
+++ T2/src/doer/main.c
@@ -3,6 +3,13 @@
 
 int main(int argc, char *argv[])
 {
+  /* El programa recibe 2 parametros */
+	if(argc != 3)
+	{
+		printf("Modo de uso: %s <file> <n>\n", argv[0]);
+		return 1;
+	}
+  
   printf("Este es el main de doer\n");
 
 }
