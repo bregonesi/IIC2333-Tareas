112abaf6b3dea167b27da65f046ee8895adae145
Benjamin <rego_1994@hotmail.com>
2018-04-13 14:18:06 -0300

    error en mallocs pendiente

--- T2/src/doer/main.c
+++ T2/src/doer/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdbool.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
@@ -21,7 +23,60 @@ int main(int argc, char *argv[])
 	}
 
   /* Lectura de archivo */
-  char *line = NULL;
+
+	char line[1024];
+	char ch = getc(archivo_tareas);
+  int index = 0;
+	int params = 0;
+	bool colon_param = false;
+	char **args = malloc(sizeof(char*) * 2);
+	while ( ch != EOF ) {
+		if(ch != '\n') {  // vamos guardando el parametro en linea caracter por caracter
+			line[index++] = ch;
+    }
+
+		if(ch == '"') {  // si hay algun parametro que use comillas
+			if(colon_param) colon_param = false;
+			else colon_param = true;
+		}
+
+		if((ch == ' ' || ch == '\n') && !colon_param) {  // guardamos nuevo parametro
+	      line[index] = '\0';
+	      index = 0;
+				params++;
+
+				if(params != 1) {  // hay que expandir args
+					printf("realloc\n");
+					args = realloc(args, sizeof(char*) * (params + 1));
+				}
+
+				printf("fds %i\n", params-1);
+				strcpy(args[params - 1], "hola\0");  // esto esta fallando
+				// strcpy(args[params - 1], line)  // aqui se pasa a una lista para que sea ejecutado por execvp
+				printf("%s\n", args[params-1]);
+
+				printf("jfldjf %s: %i\n", line, params);
+
+				if(ch == '\n') {
+					printf("fin\n");
+					for(int i = 0; i < params; i++) {
+						printf("%s\n", args[i]);
+					}
+					args[params] = NULL;
+					execvp(args[0],args);
+				}
+		}
+
+		if(ch == '\n') { // por si empieza nueva tarea
+			params = 0;
+			args = malloc(sizeof(char*) * 2);
+		}
+
+		ch = getc(archivo_tareas);
+  }
+
+
+  /*char *line = NULL;
   size_t len = 0;
   ssize_t read;
 
@@ -35,10 +90,10 @@ int main(int argc, char *argv[])
 		// commando date "+DATE: %Y-%m-%d%nTIME: %H:%M:%S"
 		char *args[]={"date", "+DATE: %Y-%m-%d%nTIME: %H:%M:%S", NULL}; // este es el caso
 		execvp(args[0],args);
-  }
+  }*/
   /* */
 
-  if (line) free(line);
+  //if (line) free(line);
 
   fclose(archivo_tareas);
 
