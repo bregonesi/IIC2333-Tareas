1147b8cab89824e497a2ff274e081c1cb04f1e12
RODRIGO ORPIS <raorpis@uc.cl>
2018-04-21 14:32:43 -0300

    arreglo bits enunciado

--- T2/src/simulator/opti.c
+++ T2/src/simulator/opti.c
@@ -5,8 +5,8 @@
 
 
 void optimo_1() {
-  int bits_1 = 22;
-  double tamano1 = (pow(2,22) * 1.375);
+  int bits_1 = 20;
+  double tamano1 = (pow(2,20) * 1.375);
   printf("----------------------------------------\n");
   printf("BITS NIVEL 1: %i\n", bits_1);
   printf("ESPACIO UTILIZADO: %g MB\n", (tamano1*(pow(10, -6))));
@@ -19,7 +19,7 @@ void optimo_2(){
   int i_min;
   double min = +INFINITY;
   double T_actual;
-  for (int i = 1; i < 22; i++) {
+  for (int i = 1; i < 20; i++) {
     T_actual = T_2(i);
     if (T_actual < min) {
       min = T_actual;
@@ -27,7 +27,7 @@ void optimo_2(){
     }
   }
   bits_1 = i_min;
-  bits_2 = 22 - i_min;
+  bits_2 = 20 - i_min;
   printf("----------------------------------------\n");
   printf("BITS NIVEL 1: %i\n", bits_1);
   printf("BITS NIVEL 2: %i\n", bits_2);
@@ -43,9 +43,9 @@ void optimo_3(){
   int j_min;
   double min = +INFINITY;
   double T_actual;
-  for (int i = 1; i < 22; i++) {
-    for (int j = 1; j < 21; j++) {
-      if ((22 - i - j) > 0) {
+  for (int i = 1; i < 20; i++) {
+    for (int j = 1; j < 19; j++) {
+      if ((20 - i - j) > 0) {
         T_actual = T_3(i, j);
         if (T_actual < min) {
           min = T_actual;
@@ -56,7 +56,7 @@ void optimo_3(){
     }
   }
   bits_1 = i_min;
-  bits_2 = 22 - i_min - j_min;
+  bits_2 = 20 - i_min - j_min;
   bits_3 = j_min;
   printf("----------------------------------------\n");
   printf("BITS NIVEL 1: %i\n", bits_1);
@@ -76,10 +76,10 @@ void optimo_4() {
   int p_min;
   double min = +INFINITY;
   double T_actual;
-  for (int i = 1; i < 22; i++) {
-    for (int j = 1; j < 21; j++) {
-      for (int p = 1; p < 20; p++) {
-        if ((22 - i - j > 2) && (j - p > 1)) {
+  for (int i = 1; i < 20; i++) {
+    for (int j = 1; j < 19; j++) {
+      for (int p = 1; p < 18; p++) {
+        if ((20 - i - j > 2) && (j - p > 1)) {
           T_actual = T_4(i, j, p);
           if (T_actual < min) {
             min = T_actual;
@@ -92,7 +92,7 @@ void optimo_4() {
     }
   }
   bits_1 = i_min;
-  bits_2 = 22 - i_min - j_min;
+  bits_2 = 20 - i_min - j_min;
   bits_3 = j_min - p_min;
   bits_4 = p_min;
   printf("----------------------------------------\n");
@@ -116,11 +116,11 @@ void optimo_5() {
   int q_min;
   double min = +INFINITY;
   double T_actual;
-  for (int i = 1; i < 22; i++) {
-    for (int j = 1; j < 21; j++) {
-      for (int p = 1; p < 20; p++) {
-        for (int q = 1; q < 19; q++) {
-          if ((22 - i - j > 2) && (j - p > 1) && (p - q > 1)) {
+  for (int i = 1; i < 20; i++) {
+    for (int j = 1; j < 19; j++) {
+      for (int p = 1; p < 18; p++) {
+        for (int q = 1; q < 17; q++) {
+          if ((20 - i - j > 2) && (j - p > 1) && (p - q > 1)) {
             T_actual = T_5(i, j, p, q);
             if (T_actual < min) {
               min = T_actual;
@@ -135,7 +135,7 @@ void optimo_5() {
     }
   }
   bits_1 = i_min;
-  bits_2 = 22 - i_min - j_min;
+  bits_2 = 20 - i_min - j_min;
   bits_3 = j_min - p_min;
   bits_4 = p_min- q_min;
   bits_5 = q_min;
@@ -150,29 +150,29 @@ void optimo_5() {
 }
 
 double T_2(int i) {
-  double k1 = powf(2, i) * ((22.0 - i) / 8.0);
-  double k2 = powf(2, 22.0 - i) * (11.0 / 8.0);
+  double k1 = powf(2, i) * ((20.0 - i) / 8.0);
+  double k2 = powf(2, 20.0 - i) * (11.0 / 8.0);
   return (k1 + k2);
 }
 
 double T_3(int i, int j) {
-  double k1 = powf(2, i) * ((22.0 - i) / 8.0);
-  double k2 = powf(2, 22.0 - i - j) * (j / 8.0);
+  double k1 = powf(2, i) * ((20.0 - i) / 8.0);
+  double k2 = powf(2, 20.0 - i - j) * (j / 8.0);
   double k3 = powf(2, j) * (11.0 / 8.0);
   return (k1 + k2 + k3);
 }
 
 double T_4(int i, int j, int p) {
-  double k1 = powf(2, i) * ((22.0 - i) / 8.0);
-  double k2 = powf(2, 22.0 - i - j) * (j / 8.0);
+  double k1 = powf(2, i) * ((20.0 - i) / 8.0);
+  double k2 = powf(2, 20.0 - i - j) * (j / 8.0);
   double k3 = powf(2, j - p) * (p / 8.0);
   double k4 = powf(2, p) * (11.0 / 8.0);
   return (k1 + k2 + k3 + k4);
 }
 
 double T_5(int i, int j, int p, int q) {
-  double k1 = powf(2, i) * ((22.0 - i) / 8.0);
-  double k2 = powf(2, 22.0 - i - j) * (j / 8.0);
+  double k1 = powf(2, i) * ((20.0 - i) / 8.0);
+  double k2 = powf(2, 20.0 - i - j) * (j / 8.0);
   double k3 = powf(2, j - p) * (p / 8.0);
   double k4 = powf(2, p - q) * (q / 8.0);
   double k5 = powf(2, q) * (11.0 / 8.0);
