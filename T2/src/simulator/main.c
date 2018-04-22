36a6d73330e85cbd07f38155f13db184482ed363
RODRIGO ORPIS <raorpis@uc.cl>
2018-04-22 02:57:11 -0300

    cosas

--- T2/src/simulator/main.c
+++ T2/src/simulator/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 #include "opti.h"
 #include "tabla.h"
 #include "funciones.h"
@@ -26,6 +27,9 @@ int main(int argc, char *argv[])
     printf("n debe ser un numero entre 1 y 5\n");
     return 1;
   }
+  int tiempo = 0; //contador para el LRU
+  int page_fault = 0;
+  int tlb_hit = 0;
   struct info_bits info;
   char****** tabla = NULL;
   char*** TLB;
@@ -37,12 +41,57 @@ int main(int argc, char *argv[])
   tabla = crear_tabla_paginas(info.b1, info.b2, info.b3, info.b4, info.b5, n);
   TLB = crear_TLB();
 
-
-  int a=54325;
-  char buffer[20];
-  itoa(a,buffer,2);   // here 2 means binary
-  printf("Binary value = %s\n", buffer);
-
+  for (linea in txt) {
+    a = linea; //tranformado a 28 bits_2
+    int pag1 = 0;
+    int pag2 = 0;
+    int pag3 = 0;
+    int pag4 = 0;
+    int pag5 = 0;
+    char pag1_c[28];
+    itoa(atoi(linea[0,info.b1]), pag1_c, 10);
+    pag1 = atoi(pag1_c);
+    if (info.b2 != 0) {
+      char pag2_c[28];
+      itoa(atoi(linea[info.b1,info.b1 + info.b2]), pag2_c, 10);
+      pag1 = atoi(pag2_c);
+    }
+    if (info.b3 != 0) {
+      char pag3_c[28];
+      itoa(atoi(linea[info.b1 + info.b2 ,info.b1 + info.b2 + info.b3]), pag3_c, 10);
+      pag1 = atoi(pag3_c);
+    }
+    if (info.b4 != 0) {
+      char pag4_c[28];
+      itoa(atoi(linea[info.b1 + info.b2 + info.b3 ,info.b1 + info.b2 + info.b3 + info.b4]), pag4_c, 10);
+      pag1 = atoi(pag4_c);
+    }
+    if (info.b5 != 0) {
+      char pag5_c[28];
+      itoa(atoi(linea[info.b1 + info.b2 + info.b3 + info.b4 ,info.b1 + info.b2 + info.b3 + info.b4 + info.b5]), pag5_c, 10);
+      pag1 = atoi(pag5_c);
+    }
+    pagina = tabla[pag1][pag2][pag3][pag4][pag5];
+    if (pagina == "-") {
+      page_fault += 1;
+      //ir a buscar a .bin y dejarlo en RAM, despues asignar el frame a esta pag y dejar seteado los extras
+      // print fisica y contenido
+    }
+    else {
+      int frame = pagina[0,8];
+      int bits_extras = pagina[8,11];
+      int ram_o_disco = bits_extras[0]; // 0 = ram, 1 = disco
+      if (ram_o_disco == 1) {
+        // buscar a disco y dejar en RAM con LRU
+        // print fisica y contenido
+      }
+      else {
+        frame = bin_to_int(frame);
+        char* arreglo = RAM[frame];
+        // print fisica y contenido
+      }
+    }
+  }
 
   return 0;
 }
